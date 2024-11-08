d44e649ec7a462cf3867715c23932d475d31c452
oussemah <oussemahamouda6@gmail.com>
2024-11-08 18:11:59 +0100

    CPM, from a simple code to a class

--- content_checker_MiniCPM/content_checker_miniCPM.py
+++ content_checker_MiniCPM/content_checker_miniCPM.py
@@ -4,29 +4,39 @@ from transformers import AutoModel, AutoTokenizer
 import base64
 from io import BytesIO
 
-def load_CPM():
-    model = AutoModel.from_pretrained("./Model", trust_remote_code=True)  
-    tokenizer = AutoTokenizer.from_pretrained("./Model", trust_remote_code=True)  
-    return model, tokenizer
+question = """
+            the included image is a scanned invoice, can u check if this data is contained in it and valid :
+            {data}
+            answer should be field_name:true/false, if it doesn't appear on the invoice check if it can be deducted from the context and set it true,
+             the response should be without comments and in JSON format
+        """
+class ContentCheckerMiniCPM:
+    def __init__(self):
+        self.model, self.tokenizer = None, None
+        self.isInitialized=False
 
-def base64_to_image_PIL(base64_image):
-    image_data = base64.b64decode(base64_image)
-    image_bytes = BytesIO(image_data)
-    image = Image.open(image_bytes)
-    return image
+    def load_CPM(self,model_path):
+        self.model = AutoModel.from_pretrained(f"./{model_path}", trust_remote_code=True)  
+        self.tokenizer = AutoTokenizer.from_pretrained(f"./{model_path}", trust_remote_code=True)  
+        self.isInitialized=True
 
-def minicpm_api(base64_image,data,model,tokenizer):
-    image = base64_to_image_PIL(base64_image)
-    question = f"""
-        the included image is a scanned invoice, can u check if this data is contained in it and valid :
-        {data}
-        answer should be field_name:true/false, if it doesn't appear on the invoice check if it can be deducted from the context and set it true,
-         the response should be without comments and in JSON format
-    """
-    msgs = [{'role': 'user', 'content': [image, question]}]
-    res = model.chat(
-        image=None,
-        msgs=msgs,
-        tokenizer=tokenizer
-        )   
-    return res,200
+    def base64_to_image_PIL(base64_image):
+        image_data = base64.b64decode(base64_image)
+        image_bytes = BytesIO(image_data)
+        image = Image.open(image_bytes)
+        return image
+    
+    def process_data(self,imageFile,data,prompt_text=question):
+        if not self.isInitialized:
+            return "Model not loaded", 400
+        image = Image.open(imageFile)
+        msgs = [{'role': 'user', 'content': [image, question.format(data=data)]}]
+        try:
+            res = self.model.chat(
+                image=None,
+                msgs=msgs,
+                tokenizer=tokenizer
+                )      
+        except Exception as e:
+            return "Error in processing data with CPM", 400
+        return res,200
