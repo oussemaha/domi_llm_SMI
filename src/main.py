eb719dab96992029b3c0ee52e587cc0f3d6f3f42
oussemah <oussemahamouda6@gmail.com>
2024-11-11 23:14:19 +0100

    first version

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 
 from content_checker_MiniCPM.content_api import CPM_api 
-
 from  SQLite_DB.db_api import db_api
 
 import base64
@@ -13,8 +12,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 app = Flask(__name__)
 app.register_blueprint(db_api,url_prefix='/db')
-#app.register_blueprint(CPM_api,url_prefix='/cpm')
-
+app.register_blueprint(CPM_api,url_prefix='/cpm')
 
 
 #API test
@@ -26,8 +24,6 @@ def test():
 
 
 if __name__=="__main__":
-
-
     app.run(host='0.0.0.0',port=5000)
 
     #run in prod mode
