2acf1d7a955c03fe2f1decc54c3c9ce2220b17f2
oussemah <oussemahamouda6@gmail.com>
2024-11-11 20:12:43 +0100

    cpm api UP

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #from src.content_checker_GPT.content_checker import gpt_api
 #from src.content_checker_MiniCPM.content_checker_miniCPM import ContentCheckerMiniCPM
-#from content_checker_MiniCPM.content_api import CPM_api 
+from content_checker_MiniCPM.content_api import CPM_api 
 
 from  SQLite_DB.Db import DB_Sqlite3
 from  SQLite_DB.db_api import db_api
@@ -22,7 +22,7 @@ Database = DB_Sqlite3()
 
 app = Flask(__name__)
 app.register_blueprint(db_api,url_prefix='/db')
-#app.register_blueprint(CPM_api,url_prefix='/cpm')
+app.register_blueprint(CPM_api,url_prefix='/cpm')
 
 
 
