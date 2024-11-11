e91fef3f9cfbd1724341e7c47768d9b86125c47c
oussemah <oussemahamouda6@gmail.com>
2024-11-11 22:32:56 +0100

    little api change

--- src/content_checker_MiniCPM/content_api.py
+++ src/content_checker_MiniCPM/content_api.py
@@ -16,7 +16,7 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@CPM_api.route('/', methods=['POST'])
+@CPM_api.route('', methods=['POST'])
 def CPM_default():
     try:
         data = json.loads(request.form.get('data'))
